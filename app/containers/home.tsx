fe6d91d6b6bbc9812e7c306f8505dbfd4d2dcf40
cescoferraro <francescoaferraro@gmail.com>
2017-05-15 16:13:19 -0300

    setting right database

--- app/containers/home.tsx
+++ app/containers/home.tsx
@@ -4,33 +4,32 @@ import RaisedButton from 'material-ui/RaisedButton';
 import { connect } from 'react-redux'
 import { firebaseConnect, dataToJS } from 'react-redux-firebase'
 import { HomeStyle } from "../css";
-
 import { push } from 'connected-react-router'
 import * as Debug from 'debug';
-
+import { isLoaded, isEmpty } from 'react-redux-firebase'
 
 @withStyles(HomeStyle)
-@firebaseConnect([
-    '/app'
-])
-@connect(
-    ({ firebase, todos }) => ({
-        app: dataToJS(firebase, '/app'),
-        todos: todos
-    })
-)
+@firebaseConnect(['/app'])
+@connect(({ firebase, todos }) => ({ app: dataToJS(firebase, '/app') }))
 export class HomeContainer extends React.Component<any, any> {
     render() {
         var debug = Debug("HomeContainer")
         debug('CREATED!');
+        debug(this.props.app);
+        const app = !isLoaded(this.props.app)
+            ? 'Loading'
+            : isEmpty(this.props.app)
+                ? 'Todo list is empty' : <h2>{this.props.app.title}</h2>
+
         return (
             <div className={HomeStyle.app}>
+                {app}
                 <h2>React-boil</h2>
                 <RaisedButton
                     onClick={() => { this.props.dispatch(push("/whatver")) }}
                     fullWidth={true}
                     label="Go Somewhere" primary={true} />
-            </div>
+            </div >
         )
     }
 }
